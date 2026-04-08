e1ac16f55bc2e06aa903070d3cdf670672935ee6
Dirison Estiven Rozo Hernández <dirirh@MacBook-Air-de-Dirison.local>
2026-04-08 15:32:58 -0500

    nueva url para el backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://parchaface-backend.redriver-74aa5bab.westus3.azurecontainerapps.io/',
   googleClientId: '163443075503-daa2pefh4uf54vaj8n7av8k260rla20e.apps.googleusercontent.com'
 };
